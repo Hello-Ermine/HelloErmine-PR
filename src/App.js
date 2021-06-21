740c20fc12a3e4644d0527da2ca171966bdc106c
boy52hz <ggwp.zero@gmail.com>
2021-06-22 02:04:33 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppContainer } from './App.style'
+import { Switch, Route } from 'react-router-dom'
 
-function App() {
+import Home from './containers/Home'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/Home/index.js
@@ -0,0 +1,11 @@
+import { HomeContainer } from './style'
+
+const Home = () => {
+  return (
+    <HomeContainer>
+      <h1>SIT - Hello World 2021</h1>
+    </HomeContainer>
+  )
+}
+
+export default Home

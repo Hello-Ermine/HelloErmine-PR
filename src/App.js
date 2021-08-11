23f0ede4a668b0be0a839e48afec960541f11ef2
boy52hz <ggwp.zero@gmail.com>
2021-08-12 03:29:55 +0700

    remove react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { AppContainer } from './App.style';
-import { Switch, Route } from 'react-router-dom';
 
 import Home from './containers/Home';
 
 const App = () => {
   return (
     <AppContainer>
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
+      <Home/>
     </AppContainer>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root')
 );
 

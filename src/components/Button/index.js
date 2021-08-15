cd012ea143aa31d8c43cf26aa8af1880c9cadeeb
Rooted <modhanami@hotmail.co.th>
2021-08-15 18:22:57 +0700

    Add hover effect for Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Button = styled.button`
+export const Button = styled.a`
   background-color: ${(props) => props.bg || 'white'};
   color: ${(props) => props.color || '#f2b6b6'};
   font-size: ${(props) => (props.large ? '36px' : '24px')};
@@ -8,4 +8,11 @@ export const Button = styled.button`
   font-family: inherit;
   font-weight: 700;
   padding: 0.4em 2em;
+  text-decoration: none;
+  transition: .2s ease-out;
+
+  :hover {
+    box-shadow: 0 0 12px rgba(255, 255, 255, 0.5),
+      0 0 100px rgba(255, 255, 255, 0.2);
+  }
 `;

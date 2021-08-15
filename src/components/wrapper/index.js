097d80b31992d189dfd42ef015fe689f86312803
Rooted <modhanami@hotmail.co.th>
2021-08-16 00:52:32 +0700

    Forward ref for Wrapper

--- src/components/wrapper/index.js
+++ src/components/wrapper/index.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
 import { StyledDiv } from './style';
 
-const Wrapper = ({ children }) => {
-  return <StyledDiv>{children}</StyledDiv>;
-};
+const Wrapper = forwardRef(function Wrapper({ children }, ref) {
+  return <StyledDiv ref={ref}>{children}</StyledDiv>;
+});
 
 Wrapper.propTypes = {
-  children: PropTypes.array
+  children: PropTypes.array,
 };
 
 export default Wrapper;

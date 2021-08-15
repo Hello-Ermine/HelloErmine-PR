b82611f8d6c6117679fc54158650c1d2c8b84d41
Thiraphat <few.thiraphat@gmail.com>
2021-08-15 23:33:06 +0700

    เพิ่มกล่องข้อความ About 😀

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,11 +1,44 @@
 import Scene from '../../components/Scene';
-import { AboutContainer } from './style';
-
+import { AboutContainer, BoxCover, BoxWithErmine, ContentInBox, NewLine, Topic , NewLineTop } from './style';
+import mascot from '../../assets/mascot3.png';
 const About = () => {
   return (
     <Scene>
       <AboutContainer>
-        <h1>About</h1>
+        {/* เป็นกล่อง สำหรับใส่ข้อความ*/}
+        <Topic>ABOUT</Topic>
+        <BoxCover>
+          <BoxWithErmine>
+            <ContentInBox>
+              <NewLineTop>
+                ยินดีต้อนรับน้องเข้าสู่โครงการ “ Hello World-Hello Ermine “
+              </NewLineTop>
+              <NewLine>
+                โครงการที่จะพาน้อง ๆ ไปตะลุยและรับความรู้ทางด้านการสร้าง
+              </NewLine>
+              <NewLine>
+                เว็บไซต์หรือเกมเพื่อเสริมสร้างทักษะการเรียนรู้ทางด้านต่าง ๆ
+              </NewLine>
+              <NewLine>
+                เพื่อที่จะนำไปต่อยอดได้จริงในอนาคต ไม่ว่าจะเป็นทางด้าน
+              </NewLine>
+              <NewLine>
+                Front-End, Web Design, Game Development, Game Design
+              </NewLine>
+              <NewLine>
+                และ Infrastructure ผ่านการสอน และเวิร์กชอปในรูปแบบต่าง ๆ
+              </NewLine>
+              <NewLine>
+                จากรุ่นพี่คณะเทคโนโลยีสารสนเทศ เตรียมตัวให้พร้อม
+              </NewLine>
+              <NewLine>
+                มาท้าความหนาวกับเหล่าเออร์มินไปด้วยกัน!!
+              </NewLine>
+            </ContentInBox>
+            <img src={mascot} />
+          </BoxWithErmine>
+        </BoxCover>
+
       </AboutContainer>
     </Scene>
   );

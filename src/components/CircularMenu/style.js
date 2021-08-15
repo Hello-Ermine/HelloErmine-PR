1f40cee296682da316be89976888c19441131e6d
Rooted <modhanami@hotmail.co.th>
2021-08-15 18:51:15 +0700

    Change CircularMenu look and feel

--- src/components/CircularMenu/style.js
+++ src/components/CircularMenu/style.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledDiv = styled.div`
-  --circular-menu-size: min(36vh, 36vw);
-  --menu-item-width: calc(var(--circular-menu-size) / 2.5);
-  --menu-item-height: calc(var(--circular-menu-size) / 6);
+  --circular-menu-size: min(28vh, 250px);
+  --menu-item-width: calc(var(--circular-menu-size) * 0.47);
+  --menu-item-height: calc(var(--circular-menu-size) * 0.2);
   width: 100vw;
   height: 100vh;
   position: absolute;
@@ -16,7 +16,7 @@ export const StyledDiv = styled.div`
     height: var(--circular-menu-size);
     top: 50%;
     right: 0;
-    transform: translate(50%, -50%);
+    transform: translate(52%, -50%);
     position: absolute;
     border-radius: 50%;
     border: 12px solid rgba(255, 255, 255, 0.555);
@@ -29,7 +29,7 @@ export const Item = styled.div`
   width: var(--menu-item-width);
   height: var(--menu-item-height);
   font-size: 2rem;
-  border-radius: 12px;
+  border-radius: 20px;
   background: rgb(231, 231, 231);
   position: absolute;
   top: 50%;
@@ -37,10 +37,11 @@ export const Item = styled.div`
   transform-origin: 100% 50%;
   transform: translate(-170%, -50%);
   opacity: ${(props) => (props.show ? 0.4 : 0)};
+  transition: .2s ease-out;
 
   &.active {
     opacity: 1;
-    box-shadow: 0 0 8px rgba(255, 255, 255, 0.5),
-      0 0 100px rgba(255, 255, 255, 0.3);
+    box-shadow: 0 0 12px rgba(255, 255, 255, 0.5),
+      0 0 100px rgba(255, 255, 255, 0.2);
   }
 `;

d197a9df2b6ea70e16059a788bfac90b89945065
Rooted <modhanami@hotmail.co.th>
2021-08-15 18:18:08 +0700

    Add Teams page content

---
+++ src/containers/Team/content.js
@@ -0,0 +1,43 @@
+import mascotWebDesign from '../../assets/teams/webdesign.png';
+import mascotFrontEnd from '../../assets/teams/frontend.png';
+import mascotInfra from '../../assets/teams/infra.png';
+import mascotGameDev from '../../assets/teams/mascot.png'; // TODO: Replace with the real one
+import mascotGameDesign from '../../assets/teams/gamedesign.png';
+
+export const content = [
+  {
+    title: 'WEB DESIGN',
+    scheme: '#9d89a2',
+    mascot: mascotWebDesign,
+    details:
+      'Web Design จะปูพื้นฐานน้อง ๆ เกี่ยวกับการออกแบบ องค์ประกอบในการออกแบบต่าง ๆ และให้ความรู้ เกี่ยวกับเรื่องของลิขสิทธิ์ รวมถึงเนื้อหาที่น่าสนใจอื่น ๆ ผ่านเวิร์กช็อปต่าง ๆ ที่พี่ ๆ ได้เตรียมไว้  เพื่อจะนำไป ต่อยอดกับการออกแบบเว็บไซต์ ที่จะถูกนำมาเป็นโปรเจกต์ที่ร่วมทำกับฝ่ายอื่น ๆ ในช่วงท้ายโครงการ',
+  },
+  {
+    title: 'FRONT END',
+    scheme: '#a8b9c9',
+    mascot: mascotFrontEnd,
+    details:
+      'ฝ่าย Front-End จะปูพื้นฐานการพัฒนาเว็บไซต์ด้วยภาษาต่าง ๆ ไม่ว่าจะเป็น HTML CSS และ JavaScript เพื่อที่จะไปต่อยอดไปกับการใช้ไลบลารี่ที่ได้รับความนิยมอย่าง React รวมถึงทักษะต่าง ๆ น้อง ๆ จะได้ สร้างเว็บไซต์ขึ้นมาจริง ๆ โดยทำงานร่วมกับฝ่ายอื่น ๆ ทำให้ได้ทักษะการทำงานร่วมกันเป็นทีมอีกด้วย',
+  },
+  {
+    title: 'INFRASTRUCTURE',
+    scheme: '#80af97',
+    mascot: mascotInfra,
+    details:
+      'สำหรับฝ่าย Infrastructure น้อง ๆ จะเรียนรู้วิธีที่ทำให้ผู้คนภายนอกเข้าชม website ผ่าน domain ที่เราตั้งได้ น้อง ๆ จะได้รู้จักวิธีการใช้ Command Line user interface หรือ CLI และหัวข้อที่เกี่ยวข้องอื่น ๆ รวมถึงได้เสริมทักษะการทำงานร่วมกันเป็นทีมอีกด้วย',
+  },
+  {
+    title: 'GAME DEVELOPMENT',
+    scheme: '#cdc191',
+    mascot: mascotGameDev,
+    details:
+      'ฝ่าย Game Development จะพาน้อง ๆ ไปเรียนรู้เกี่ยวกับการสร้างเกมอย่างแท้จริง ว่าเกมบน เว็บบราวเซอร์ มีองค์ประกอบอะไรบ้าง และน้อง ๆ จะได้มีโอกาสสร้างเกมของตัวเองที่สามารถ เล่นได้จริงบนเว็บไซต์ รับรองว่าทั้งสนุก และอัดแน่นไปด้วยความรู้อย่างแน่นอน',
+  },
+  {
+    title: 'GAME DESIGN',
+    scheme: '#dd93a9',
+    mascot: mascotGameDesign,
+    details:
+      'เมื่อเข้ามาในฝ่าย Game Design น้อง ๆ จะได้เรียนรู้ถึงหลักการ วิธีการออกแบบ และปัจจัยในการออกแบบ เกมให้มีความน่าสนใจ รวมถึงองค์ประกอบของเกมเกมหนึ่งด้วย น้อง ๆ จะได้สร้างตัวละคร รวมถึง Game Play ที่ผ่านจินตนาการของน้อง ๆ และจะถูกนำไปใช้ในการสร้างเกมจริง ๆ อีกด้วย',
+  },
+];

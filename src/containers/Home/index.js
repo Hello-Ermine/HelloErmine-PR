6aa229e3b45ff0dbbbc2ef65a900c4958f3390f2
boy52hz <ggwp.zero@gmail.com>
2021-08-25 21:54:01 +0700

    added: home background element

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,15 @@
 import Scene from '../../components/Scene';
-import { HomeContainer, HomeContent, Logo, CI } from './style';
+import { HomeContainer, HomeContent, Logo, CI, BackgroundElement, TreeElement1, TreeElement2, TreeElement3 } from './style';
 import { Caption } from '../../components/Typography';
 import Particles from 'react-tsparticles';
 
 import ErmineLogo from '../../assets/hello-ermine_logo.png';
 import CIImage from '../../assets/CI.png';
+
+import FrontScene from '../../assets/home/home_background1.png';
+import Tree1 from '../../assets/home/home_tree1.png';
+import Tree2 from '../../assets/home/home_tree2.png';
+import Tree3 from '../../assets/home/home_tree3.png';
 
 const Home = () => {
   return (
@@ -15,7 +20,10 @@ const Home = () => {
           <Caption>Break your limit, practice your skills</Caption>
         </HomeContent>
         <CI src={CIImage} alt='CI Image' />
-
+        <TreeElement1 src={Tree1}/>
+        <TreeElement2 src={Tree2}/>
+        <TreeElement3 src={Tree3}/>
+        <BackgroundElement src={FrontScene}/>
       </HomeContainer>
       <Particles
         id='snow'
--- src/containers/Home/style.js
+++ src/containers/Home/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Background from '../../assets/home/home_background.png';
+import Background from '../../assets/home/home_background2.png';
 
 export const HomeContainer = styled.section`
   position: relative;
@@ -7,7 +7,7 @@ export const HomeContainer = styled.section`
   height: 100%;
   width: 100%;
   background-image: url(${Background});
-  background-position: center;
+  background-position: 0 85%;
   background-repeat: no-repeat;
   background-size: cover;
 `;
@@ -20,6 +20,7 @@ export const HomeContent = styled.div`
   text-align: center;
   width: 100%;
   line-height: 0;
+  z-index: 6;
 
   p {
     font-weight: 500;
@@ -37,4 +38,39 @@ export const CI = styled.img`
   bottom: 38px;
   width: 219px;
   height: 71px;
+  z-index: 5;
+`;
+
+export const BackgroundElement = styled.img`
+  position: absolute;
+  width: 100%;
+  z-index: 4;
+  bottom: 0;
+  transform: translateY(65%);
+`;
+
+export const TreeElement1 = styled.img`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  transform: translateX(-45%);
+  width: 45%;
+  z-index: 3;
+`;
+
+export const TreeElement2 = styled.img`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 30%;
+  transform: translate(0, -10%);
+  z-index: 2;
+`;
+
+export const TreeElement3 = styled.img`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 25%;
+  transform: translate(30%, -40%);
 `;

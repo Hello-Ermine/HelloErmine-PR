a815f2f9e67282a4a387fe04233af1e53b03366a
Rooted <modhanami@hotmail.co.th>
2021-08-25 13:38:06 +0700

    Add Teams page content transition

--- src/containers/Team/index.js
+++ src/containers/Team/index.js
@@ -2,21 +2,38 @@ import Scene from '../../components/Scene';
 import { Content, Details, TeamContainer, Title, TeamContent } from './style';
 
 import { CircularMenu } from '../../components/CircularMenu';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Button } from '../../components/Button';
 import { content as contentData } from './content';
+import { gsap } from 'gsap';
 
 const Team = () => {
   const [content, setContent] = useState(contentData[2]);
+  const teamContentRef = useRef(null);
+
+  const handleUpdateIndex = (index) => {
+    const teamContent = teamContentRef.current;
+    const newContent = contentData[index];
+
+    gsap.to(teamContent, {
+      autoAlpha: 0,
+      duration: 0.25,
+      onComplete: () => {
+        setContent(newContent);
+        gsap.to(teamContent, {
+          autoAlpha: 1,
+          duration: 0.25,
+        });
+      },
+    });
+  };
 
   return (
     <Scene>
       <TeamContainer>
         <CircularMenu
           angle="80"
-          onUpdateIndex={(index) => {
-            setContent(contentData[index]);
-          }}
+          onUpdateIndex={handleUpdateIndex}
         >
           <div></div>
           <div></div>
@@ -25,7 +42,7 @@ const Team = () => {
           <div></div>
         </CircularMenu>
         <Content>
-          <TeamContent>
+          <TeamContent ref={teamContentRef}>
             <img src={content.mascot} alt={content.title} />
             <Title color={content.scheme}>{content.title}</Title>
             <Details>{content.details}</Details>

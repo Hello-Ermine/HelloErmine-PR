4270f356325178735ef3f3aa5c1544ab75c3fa1a
Rooted <modhanami@hotmail.co.th>
2021-08-27 20:31:57 +0700

    kinda works huh

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ const data = {
   isChanging: false,
   st: null,
   tl: null,
+  tween: null,
+  tweenBlackScreen: null,
 };
 
 const throttle = (fn, delay) => {
@@ -37,9 +39,43 @@ const duration = .5;
 const App = () => {
   const wrapperRef = useRef(null);
   const blackScreenRef = useRef(null);
-  const [isChanging, setIsChanging] = useState(false);
-  const [pageIndex, setPageIndex] = useState(0);
-  const [scrollTriggerInstance, setScrollTriggerInstance] = useState(null);
+  const [pageIndex, _setPageIndex] = useState(0);
+
+  const setPageIndex = (index) => {
+    data.pageIndex = index;
+    _setPageIndex(index);
+  };
+
+  const handleUpdateIndex = (index) => {
+    const wrapper = wrapperRef.current;
+
+    data.st.scroll(index * wrapper.offsetWidth);
+
+    if (data.tween?.isActive()) {
+      console.log("KILL");
+      data.tween.kill();
+    }
+
+    setPageIndex(index);
+
+    data.tween = data.tl.tweenTo(Object.values(data.tl.labels)[index], {
+      duration,
+    });
+  };
+
+  const throttledHandleUpdateIndex = throttle(handleUpdateIndex, duration * 1000);
+
+  const handleUpdateScrollTrigger = (st) => {
+    const wrapper = wrapperRef.current;
+    const targets = wrapper.childNodes;
+
+    const pageIndex = data.pageIndex;
+    const nextIndex = Math.min(Math.max(0, pageIndex + st.direction), targets.length - 1);
+
+    throttledHandleUpdateIndex(nextIndex);
+  };
+
+  const throttledHandleUpdateScrollTrigger = throttle(handleUpdateScrollTrigger, duration * 1000);
 
   useEffect(() => {
     const wrapper = wrapperRef.current;
@@ -47,76 +83,26 @@ const App = () => {
     const targets = wrapper.childNodes;
     const multiplier = targets.length - 1;
 
-    const handleUpdate = (st) => {
-      // console.log(data.tween?.progress());
-      console.log("TRIGGERED");
-      // console.log(st.enabled);
-      if (!data.st) {
-        return;
-      }
-
-      const pageIndex = data.pageIndex + (st.direction === 1 ? 1 : 0); // EnterBack (equals to direction = -1) had already been triggered by the time this is called
-      const nextIndex = Math.min(Math.max(0, pageIndex), targets.length - 1);
-
-      console.log("Update", nextIndex);
-      // data.isChanging = true;
-      // if (data.tween) {
-      //   return;
-      // }
-      // data.tween = gsap.to(window, {
-      //   scrollTo: nextIndex * wrapper.offsetWidth,
-      //   duration,
-      //   onComplete: () => {
-      //     console.log("DONE");
-      //     data.tween = null;
-      //   },
-      // });
-
-      data.st.scroll(nextIndex * wrapper.offsetWidth);
-      if (data.tween) {
-        data.tween.kill();
-      }
-      data.tween = data.tl.tweenTo(Object.values(data.tl.labels)[nextIndex], {
-        duration, 
-      });
-      // data.st.tweenTo(nextIndex * wrapper.offsetWidth);
-
-      // data.pageIndex = nextIndex;
-      // setPageIndex(nextIndex);
-    };
-
-    const throttledHandleUpdate = throttle(handleUpdate, duration * 1000);
-
     const tl = gsap.timeline({
       defaults: {
         ease: 'none',
       },
       scrollTrigger: {
         trigger: wrapper,
-        // scrub: true,
         end: () => `+=${wrapper.offsetWidth * multiplier}`,
         pin: true,
-        // snap: {
-        //   snapTo: "labels",
-        //   duration,
-        //   delay: 0.1,
-        //   inertia: false,
-        //   ease: 'none',
-        //   onComplete: () => {
-        //     console.log("Snapped");
-        //   }
-        // },
         onUpdate: (self) => {
-          // if (data.tween) {
-          //   return;
-          // }
-
-          throttledHandleUpdate(self);
-          // handleUpdate(self);
+          if (!data.st || data.isChanging) {
+            return;
+          }
+          // if (self.
+          console.log("UPDATE");
+          throttledHandleUpdateScrollTrigger(self);
         },
       },
-    }).pause();
+    });
 
+    tl.pause();
     const st = tl.scrollTrigger;
 
     targets.forEach((target, i, targets) => {
@@ -124,24 +110,6 @@ const App = () => {
         trigger: target,
         start: () => `top top-=${target.offsetWidth * i - 1}`,
         end: () => `top top-=${target.offsetWidth * (i + 1) - 1}`,
-        onEnter: () => {
-          // if (data.isChanging) {
-          //   return;
-          // }
-          data.pageIndex = i;
-          // console.log(data.pageIndex);
-          setPageIndex(i);
-          // console.log("Enter", i);
-        },
-        onEnterBack: () => { 
-          // if (data.isChanging) {
-          //   return;
-          // }
-          data.pageIndex = i;
-          // console.log(data.pageIndex);
-          setPageIndex(i);
-          // console.log("EnterBack", i);
-        },
       });
 
       const xPercentEnterSet = -100 * (i - 1);
@@ -178,47 +146,37 @@ const App = () => {
 
     data.tl = tl;
     data.st = st;
-    setScrollTriggerInstance(st);
   }, []);
 
   const handlePageAnchor = (index) => {
-    if (pageIndex === index || isChanging) {
+    if (pageIndex === index || data.isChanging) {
       return;
     }
 
-    // const scrub = scrollTriggerInstance.getTween();
-    // const snap = scrollTriggerInstance.getTween(true);
+    data.isChanging = true;
 
-    // if (scrub) {
-    //   scrub.kill();
-    // }
-    // if (snap) {
-    //   snap.kill();
-    // }
+    const wrapper = wrapperRef.current;
+    const blackScreen = blackScreenRef.current;
 
-    // setIsChanging(true);
+    data.st.scroll(index * wrapper.offsetWidth);
+    // Set data.isChanging to false earlier and the whole thing collapses
+    data.tweenBlackScreen = gsap.to(blackScreen, {
+      autoAlpha: 1,
+      duration: 0.25,
+    }).then(() => {
+      data.tl.seek(`page-${index}`);
+      gsap.fromTo(blackScreen, {
+        autoAlpha: 1,
+      }, {
+        autoAlpha: 0,
+        duration: .5,
+        onComplete: () => {
+          data.isChanging = false;
+        },
+      });
+    });
 
-    // const wrapper = wrapperRef.current;
-    // const blackScreen = blackScreenRef.current;
-
-    // gsap.to(blackScreen, {
-    //   autoAlpha: 1,
-    //   duration: 0.25,
-    //   onComplete: () => {
-    //     timeline.seek(`page-${index}`);
-    //     scrollTriggerInstance.scroll(index * wrapper.offsetWidth);
-    //     gsap.fromTo(blackScreen, {
-    //       autoAlpha: 1,
-    //     }, {
-    //       autoAlpha: 0,
-    //       duration: 0.25,
-    //       onComplete: () => {
-    //         setPageIndex(index);
-    //         setIsChanging(false);
-    //       },
-    //     });
-    //   }
-    // });
+    setPageIndex(index);
   };
 
   return (

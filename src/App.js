3995fea9ac10a650ebfb3aabcc467ab78c896d1b
Rooted <modhanami@hotmail.co.th>
2021-08-27 12:51:40 +0700

    H

--- src/App.js
+++ src/App.js
@@ -9,8 +9,29 @@ import { AppSocial, BlackScreen } from './App.style';
 import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+const data = {
+  pageIndex: 0,
+  isChanging: false,
+  st: null,
+};
+
+const throttle = (fn, delay) => {
+  let previous = 0;
+
+  return (...args) => {
+    const now = Date.now();
+    if (previous + delay < now) {
+      previous = now;
+      fn(...args);
+    }
+  };
+};
+
+const duration = .5;
 
 const App = () => {
   const wrapperRef = useRef(null);
@@ -26,21 +47,66 @@ const App = () => {
     const targets = wrapper.childNodes;
     const multiplier = targets.length - 1;
 
+    const handleUpdate = (st) => {
+      // console.log(data.tween?.progress());
+      console.log("TRIGGERED");
+      // console.log(st.enabled);
+      if (!data.st) {
+        return;
+      }
+
+      const pageIndex = data.pageIndex + (st.direction === 1 ? 1 : 0); // EnterBack (equals to direction = -1) had already been triggered by the time this is called
+      const nextIndex = Math.min(Math.max(0, pageIndex), targets.length - 1);
+
+      console.log("Update", nextIndex);
+      // data.isChanging = true;
+      // if (data.tween) {
+      //   return;
+      // }
+      // data.tween = gsap.to(window, {
+      //   scrollTo: nextIndex * wrapper.offsetWidth,
+      //   duration,
+      //   onComplete: () => {
+      //     console.log("DONE");
+      //     data.tween = null;
+      //   },
+      // });
+
+      data.st.scroll(nextIndex * wrapper.offsetWidth);
+      // data.st.tweenTo(nextIndex * wrapper.offsetWidth);
+      
+      // data.pageIndex = nextIndex;
+      // setPageIndex(nextIndex);
+    };
+
+    const throttledHandleUpdate = throttle(handleUpdate, duration * 1000);
+
     const tl = gsap.timeline({
       defaults: {
         ease: 'none',
       },
       scrollTrigger: {
         trigger: wrapper,
-        scrub: 0.5,
+        scrub: true,
         end: () => `+=${wrapper.offsetWidth * multiplier}`,
         pin: true,
-        snap: {
-          snapTo: "labelsDirectional",
-          duration: 0.5,
-          delay: 0.1,
-          inertia: false,
-          ease: 'none',
+        // snap: {
+        //   snapTo: "labels",
+        //   duration,
+        //   delay: 0.1,
+        //   inertia: false,
+        //   ease: 'none',
+        //   onComplete: () => {
+        //     console.log("Snapped");
+        //   }
+        // },
+        onUpdate: (self) => {
+          // if (data.tween) {
+          //   return;
+          // }
+
+          throttledHandleUpdate(self);
+          // handleUpdate(self);
         },
       },
     });
@@ -51,8 +117,24 @@ const App = () => {
         trigger: target,
         start: () => `top top-=${target.offsetWidth * i - 1}`,
         end: () => `top top-=${target.offsetWidth * (i + 1) - 1}`,
-        onEnter: () => setPageIndex(i),
-        onEnterBack: () => setPageIndex(i),
+        onEnter: () => {
+          // if (data.isChanging) {
+          //   return;
+          // }
+          data.pageIndex = i;
+          // console.log(data.pageIndex);
+          setPageIndex(i);
+          // console.log("Enter", i);
+        },
+        onEnterBack: () => { 
+          // if (data.isChanging) {
+          //   return;
+          // }
+          data.pageIndex = i;
+          // console.log(data.pageIndex);
+          setPageIndex(i);
+          // console.log("EnterBack", i);
+        },
       });
 
       const xPercentEnterSet = -100 * (i - 1);
@@ -88,6 +170,7 @@ const App = () => {
     });
 
     setTimeline(tl);
+    data.st = st;
     setScrollTriggerInstance(st);
   }, []);
 
@@ -96,39 +179,39 @@ const App = () => {
       return;
     }
 
-    const scrub = scrollTriggerInstance.getTween();
-    const snap = scrollTriggerInstance.getTween(true);
+    // const scrub = scrollTriggerInstance.getTween();
+    // const snap = scrollTriggerInstance.getTween(true);
 
-    if (scrub) {
-      scrub.kill();
-    }
-    if (snap) {
-      snap.kill();
-    }
+    // if (scrub) {
+    //   scrub.kill();
+    // }
+    // if (snap) {
+    //   snap.kill();
+    // }
 
-    setIsChanging(true);
+    // setIsChanging(true);
 
-    const wrapper = wrapperRef.current;
-    const blackScreen = blackScreenRef.current;
+    // const wrapper = wrapperRef.current;
+    // const blackScreen = blackScreenRef.current;
 
-    gsap.to(blackScreen, {
-      autoAlpha: 1,
-      duration: 0.25,
-      onComplete: () => {
-        timeline.seek(`page-${index}`);
-        scrollTriggerInstance.scroll(index * wrapper.offsetWidth);
-        gsap.fromTo(blackScreen, {
-          autoAlpha: 1,
-        }, {
-          autoAlpha: 0,
-          duration: 0.25,
-          onComplete: () => {
-            setPageIndex(index);
-            setIsChanging(false);
-          },
-        });
-      }
-    });
+    // gsap.to(blackScreen, {
+    //   autoAlpha: 1,
+    //   duration: 0.25,
+    //   onComplete: () => {
+    //     timeline.seek(`page-${index}`);
+    //     scrollTriggerInstance.scroll(index * wrapper.offsetWidth);
+    //     gsap.fromTo(blackScreen, {
+    //       autoAlpha: 1,
+    //     }, {
+    //       autoAlpha: 0,
+    //       duration: 0.25,
+    //       onComplete: () => {
+    //         setPageIndex(index);
+    //         setIsChanging(false);
+    //       },
+    //     });
+    //   }
+    // });
   };
 
   return (

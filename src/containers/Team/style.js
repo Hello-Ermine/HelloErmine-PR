3b0cb232f375c5e1ddcef97d4939cc709db507e5
Rooted <modhanami@gmail.com>
2021-09-02 21:37:35 +0700

    Make Teams responsive

--- src/containers/Team/style.js
+++ src/containers/Team/style.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components/macro';
 import background from '../../assets/teams/team_background1.jpg';
 import background2 from '../../assets/teams/team_background2.png';
+import { downSizes, upSizes } from '../../constants/breakpoints';
 
 export const TeamContainer = styled.section`
   background-color: #1e1f21;
   height: 100%;
   width: 100%;
   background-image: url(${background});
-  background-position: 0 64%;
+  background-position: 40% 64%;
   background-size: cover;
 `;
+
 
 export const Content = styled.div`
   position: relative;
@@ -20,18 +22,38 @@ export const Content = styled.div`
   justify-content: center;
   align-items: center;
   margin: 0 auto;
+  margin-top: 2%;
   background-image: url(${background2});
-  background-size: 90vh;
-  background-position: 52% -22%;
+  background-size: 76vh;
+  background-position: 52% calc(-50% + 8.5vw);
   background-repeat: no-repeat;
 
+  @media (orientation: portrait) {
+    background-size: 92vw;
+    background-position: -11vw calc(50% - 25vw);
+    align-items: flex-start;
+    padding-left: 6.25%;
+  }
+  
+  @media ${downSizes.xs} and (orientation: portrait) {
+    background-position: -11vw calc(50% - 120px);
+  }
+
+  @media ${upSizes.sm} and (orientation: portrait) {
+    background-position: -11vw calc(50% - 18vw);
+  }
+
+  @media ${downSizes.xs} and (orientation: landscape) {
+    background-size: 64vh;
+    background-position: 52% calc(50% - 150px);
+  }
 `;
 
 // TODO: Create a mascot container and use background2 as a bg there.
 
 export const Mascot = styled.img`
   width: ${props => props.portrait ? '30vh' : '40vh'};
-    animation: fold-ermine 5s ease infinite both;
+  animation: fold-ermine 5s ease infinite both;
 
   @media (orientation: portrait) {
     width: ${props => props.portrait ? '30vw' : '40vw'};
@@ -53,6 +75,11 @@ export const Title = styled.div`
   font-size: 48px;
   font-weight: 700;
   color: ${(props) => props.color || 'black'};
+
+  @media ${downSizes.xs} and (orientation: portrait) {
+    font-size: 18px;
+    margin-bottom: 8px;
+  }
 `;
 
 export const Details = styled.div`
@@ -63,6 +90,12 @@ export const Details = styled.div`
   text-align: left;
   line-height: 2rem;
   word-spacing: 2px;
+
+  @media ${downSizes.xs} and (orientation: portrait) {
+    font-size: 9px;
+    line-height: 2em;
+    text-indent: 1.6em;
+  }
 `;
 
 export const Scroll = styled.div`
@@ -113,5 +146,16 @@ export const Scroll = styled.div`
     background-size: 100%;
     background-repeat: no-repeat;
     visibility: ${props => props.active ? 'visible' : 'hidden'};
+  }
+
+  @media ${downSizes.xs} {
+    width: 24vw;
+    height: 14vw;
+  }
+
+  
+  @media ${downSizes.lg} and (orientation: landscape) {
+    width: 16vh;
+    height: 8.8vh;
   }
 `;

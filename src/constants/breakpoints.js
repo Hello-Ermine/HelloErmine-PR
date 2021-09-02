596e5abb2993463f5f4b79d1040d5424596bcbdf
Rooted <modhanami@gmail.com>
2021-09-02 21:09:31 +0700

    Add breakpoint constants

---
+++ src/constants/breakpoints.js
@@ -0,0 +1,15 @@
+export const downSizes = {
+  xs: '(max-width: 575.98px)',
+  sm: '(max-width: 767.98px)',
+  md: '(max-width: 991.98px)',
+  lg: '(max-width: 1199.98px)',
+  xl: '(max-width: 1399.98px)',
+};
+
+export const upSizes = {
+  xs: '(min-width: 576px)',
+  sm: '(min-width: 768px)',
+  md: '(min-width: 992px)',
+  lg: '(min-width: 1200px)',
+  xl: '(min-width: 1400px)',
+};

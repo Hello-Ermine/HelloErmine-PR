135b31bf26cb6fb85594961bf8f90e85479ad481
modhanami <modhanami@gmail.com>
2021-09-15 23:49:15 +0700

    Cleanup App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Game from './containers/Game';
 import Wrapper from './components/Wrapper';
 import Navbar from './components/Navbar';
 import { AppSocial, BlackScreen } from './App.style';
-import { useEffect, useRef, useState, useMemo } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
@@ -15,53 +15,6 @@ import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const scrollHeightMultiplier = 6;
-
-const getLabel = (index) => {
-  return `page-${index}`;
-};
-
-const killScrollTriggerTweens = (st) => {
-  const scrub = st.getTween();
-  const snap = st.getTween(true);
-
-  if (scrub) {
-    scrub.kill();
-  }
-  if (snap) {
-    snap.kill();
-  }
-};
-
-const scrollCompleteCallback = (() => {
-  let interval = null;
-
-  return (func, overwrite = true) => {
-    let previousY = window.scrollY;
-
-    if (overwrite) {
-      clearInterval(interval);
-    }
-    
-    interval = setInterval(() => {
-      if (previousY === window.scrollY) {
-        clearInterval(interval);
-        func();
-      }
-      previousY = window.scrollY;
-    }, 100);
-  }; 
-})();
-
-const debounce = (func, delay) => {
-  let timeout = null;
-
-  return (...args) => {
-    clearTimeout(timeout);
-
-    timeout = setTimeout(() => func(...args), delay);
-  };
-};
-
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 const App = () => {
@@ -73,10 +26,7 @@ const App = () => {
   const wrapperRef = useRef(null);
   const blackScreenRef = useRef(null);
   const dataRef = useRef({
-    isResizing: false,
     isProgressing: false,
-    isLoading: true,
-    snapInterval: null,
     pageIndex,
     lastScrollY: null,
     touchStartY: null,
@@ -88,6 +38,10 @@ const App = () => {
     dataRef.current.pageIndex = index;
     _setPageIndex(index);
   };
+
+  const getLabel = useCallback((index) => {
+    return `page-${index}`;
+  }, []);
 
   const fadeBlack = (onFadeIn, onFadeOut, durationMs = 500) => {
     const blackScreen = blackScreenRef.current;
@@ -122,89 +76,22 @@ const App = () => {
     const targets = wrapper.childNodes;
     const multiplier = targets.length - 1;
 
-    // const getPagesScrollTop = () => {
-    //   return Array(targets.length)
-    //     .fill(0)
-    //     .map((_, i) => window.innerWidth * scrollHeightMultiplier * i);
-    // };
-
-    // dataRef.current.pagesScrollTop = getPagesScrollTop();
     const closestPage = () => {
-      return Math.round(window.scrollY / (window.innerWidth * scrollHeightMultiplier * multiplier) * multiplier);
+      return Math.round(window.scrollY / (window.innerWidth * scrollHeightMultiplier));
     };
 
-    console.log(dataRef.current.pagesScrollTop);
     const tl = gsap.timeline({
       defaults: {
         ease: 'none',
       },
     });
-
     tl.pause();
-
-    const handleAfterProgressing = () => {
-      clearInterval(dataRef.current.snapInterval);
-      dataRef.current.isProgressing = false;
-    };
-
-    const debouncedHandleAfterProgressing = debounce(handleAfterProgressing, 500);
-
-    const supportsTouch = 'ontouchstart' in window || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
 
     const st = ScrollTrigger.create({
       trigger: wrapper,
       end: () => `+=${window.innerWidth * scrollHeightMultiplier * multiplier}`,
       pin: true,
-      onUpdate: (st) => {
-        // if (dataRef.current.isProgressing || dataRef.current.isLoading) {
-        //   return;
-        // }
-
-        // const direction = st.direction;
-        // const currentIndex = dataRef.current.pageIndex;
-        // const nextIndex = currentIndex + direction;
-        
-        // if (nextIndex < 0 || nextIndex > targets.length - 1) {
-        //   return;
-        // }
-
-        // dataRef.current.isProgressing = true;
-
-        // // if supports touch, scroll to the extreme end of the direction
-        // if (supportsTouch) {
-        //   if (direction === 1) {
-        //     st.scroll(window.innerWidth * scrollHeightMultiplier * multiplier);
-        //   } else {
-        //     st.scroll(0);
-        //   }
-        // }
-        
-        // setTimeout(() => {
-        //   dataRef.current.snapInterval = setInterval(() => {
-        //     st.scroll(nextIndex * window.innerWidth * scrollHeightMultiplier);
-        //   }, 50);
-        // }, 50);
-
-        // fadeBlack(() => {
-        //   tl.seek(getLabel(nextIndex));
-        //   st.scroll(nextIndex * window.innerWidth * scrollHeightMultiplier);
-        //   setPageIndex(nextIndex);
-        // },
-        // () => {
-        //   if (supportsTouch) {
-        //     scrollCompleteCallback(() => {
-        //       setTimeout(() => {
-        //         handleAfterProgressing();
-        //       }, 100);
-        //     });
-        //   } else {
-        //     debouncedHandleAfterProgressing();
-        //   }
-        // }, 1000);
-      },
     });
-
-    // st.disable();
 
     targets.forEach((target, i, targets) => {
       const xPercentEnterSet = -100 * (i - 1); // left: 100%
@@ -235,62 +122,6 @@ const App = () => {
       tl.to(target, {
         xPercent: xPercentExitTo,
       }, "+=.5");
-    });
-
-    // const debouncedHandleRefresh = debounce(() => {
-    //   dataRef.current.isResizing = false;
-    // }, 1500);
-
-    // ScrollTrigger.addEventListener('refreshInit', () => {
-    //   dataRef.current.isResizing = true;
-    //   debouncedHandleRefresh();
-    // });
-    
-    // const debouncedDisableIsLoading = debounce(() => {
-    //   dataRef.current.isLoading = false;
-    // }, 1500);
-
-    // ScrollTrigger.addEventListener('refresh', () => {
-    //   killScrollTriggerTweens(st);
-    //   st.scroll(dataRef.current.pageIndex * window.innerWidth * scrollHeightMultiplier);
-    //   st.enable();
-    //   debouncedDisableIsLoading();
-    // });
-
-    // reset scroll position to 0 after a refresh
-    // window.addEventListener('beforeunload', () => {
-    //   st.disable();
-    // });   
-
-    // prevent scroll trigger from going brrr on touchscreen devices
-    // window.addEventListener('touchmove', (e) => {
-    //   if (e.cancelable && dataRef.current.isProgressing) {
-    //     e.preventDefault();
-    //   }
-    // }, { passive: false });
-
-    // window.addEventListener('wheel', (e) => {
-    //   if (dataRef.current.isProgressing) {
-    //     e.preventDefault();
-    //   }
-    // }, { passive: false });
-    
-    window.addEventListener('scroll', (_) => {
-      // if (!dataRef.current.lastScrollY) {
-      //   dataRef.current.lastScrollY = scrollY;
-      //   return;
-      // }
-      // const scrollY = window.scrollY;
-      // const scrollDeltaY = scrollY - dataRef.current.lastScrollY;
-      // dataRef.current.lastScrollY = scrollY;
-
-      // if (scrollDeltaY > 0) {
-      //   handleSceneChanging(1);
-      // } else {
-      //   handleSceneChanging(-1);
-      // }
-
-      // return;
     });
 
     const changeScene = (index, autoKill = false) => {
@@ -345,16 +176,14 @@ const App = () => {
 
       const deltaY = e.touches[0].clientY - dataRef.current.touchStartY;
       const threshold = 20;
-      
+      const onChange = () => {
+        dataRef.current.touchStartY = e.touches[0].clientY;
+      };
       if (deltaY > threshold) {
-        handleScrollDirection(-1, () => {
-          dataRef.current.touchStartY = e.touches[0].clientY;
-        });
+        handleScrollDirection(-1, onChange);
       }
       if (deltaY < -threshold) {
-        handleScrollDirection(1, () => {
-          dataRef.current.touchStartY = e.touches[0].clientY;
-        });
+        handleScrollDirection(1, onChange);
       }
     };
 
@@ -370,6 +199,7 @@ const App = () => {
       if (dataRef.current.isProgressing) {
         return;
       }
+
       if (['ArrowUp', 'PageUp', 'w', 'i'].includes(e.key)) {
         handleScrollDirection(-1);
       }
@@ -393,7 +223,6 @@ const App = () => {
       if (dataRef.current.pageIndex === nextIndex) {
         return;
       }
-      console.log(dataRef.current.pageIndex, nextIndex);
       changeScene(nextIndex, true);
     };
 
